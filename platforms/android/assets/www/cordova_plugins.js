06dd28b36896a5d64048853979b1f69b5873c2a7
chaasfr <chaasfr@gmail.com>
2015-06-08 14:18:12 +0200

    fixed connectionfailed

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,6 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.albahra.plugin.networkinterface/www/networkinterface.js",
+        "id": "com.albahra.plugin.networkinterface.networkinterface",
+        "clobbers": [
+            "window.networkinterface"
+        ]
+    },
+    {
+        "file": "plugins/com.pylonproducts.wifiwizard/www/WifiWizard.js",
+        "id": "com.pylonproducts.wifiwizard.DynamicUpdate",
+        "clobbers": [
+            "WifiWizard"
+        ]
+    },
+    {
         "file": "plugins/com.randdusing.bluetoothle/www/bluetoothle.js",
         "id": "com.randdusing.bluetoothle.BluetoothLe",
         "clobbers": [
@@ -49,33 +68,20 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
-    },
-    {
-        "file": "plugins/com.pylonproducts.wifiwizard/www/WifiWizard.js",
-        "id": "com.pylonproducts.wifiwizard.DynamicUpdate",
-        "clobbers": [
-            "WifiWizard"
-        ]
-    },
-    {
-        "file": "plugins/com.albahra.plugin.networkinterface/www/networkinterface.js",
-        "id": "com.albahra.plugin.networkinterface.networkinterface",
-        "clobbers": [
-            "window.networkinterface"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.albahra.plugin.networkinterface": "1.0.7",
+    "com.pylonproducts.wifiwizard": "0.2.8",
     "com.randdusing.bluetoothle": "2.1.0",
+    "com.tsubik.cordova.start_on_boot": "1.0.2",
     "com.verso.cordova.clipboard": "0.1.0",
     "org.apache.cordova.device": "0.3.0",
     "org.apache.cordova.dialogs": "0.3.0",
-    "org.apache.cordova.network-information": "0.2.15",
-    "com.pylonproducts.wifiwizard": "0.2.8",
-    "com.tsubik.cordova.start_on_boot": "1.0.2",
-    "com.albahra.plugin.networkinterface": "1.0.7"
+    "org.apache.cordova.network-information": "0.2.15"
 }
 // BOTTOM OF METADATA
 });
